503817624d96e5574f6bafefd737a397d5560ba5
PritishWadhavane <pritishwadhavane1@gmail.com>
2026-07-21 18:40:05 +0530

    21July session

---
+++ PackageExample.java
@@ -0,0 +1,7 @@
+import message.GetMessage;
+public class PackageExample {
+    public static void main(String[] args) {
+        GetMessage obj = new GetMessage();
+        obj.displayMethod();
+    }
+}
---
+++ message/GetMessage.java
@@ -0,0 +1,6 @@
+package message;
+public class GetMessage {
+    public void displayMethod(){
+        System.out.println("I am user defined method!!");
+    }
+}
