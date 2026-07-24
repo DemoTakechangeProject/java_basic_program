1ff059ca91e802411455b1adc9bc60f23d00c4cb
PritishWadhavane <pritishwadhavane1@gmail.com>
2026-07-24 13:00:03 +0530

    24 extra session

---
+++ WhileExample.java
@@ -0,0 +1,16 @@
+public class WhileExample {
+    public static void main(String[] args) {
+        int i=1;
+//        while(i<=10)// 11<=10
+//        {
+//            System.out.print(i); // 1 2 3 4 5 6 7 8 9 10
+//            i++; // i=i+1  i=1+1  i=2
+//            System.out.println(" "+i);
+//        }
+        for(int a=1;a<=10;a++) {
+            System.out.print(a);
+            a++;
+            System.out.println(" "+a);
+        }
+    }
+}
