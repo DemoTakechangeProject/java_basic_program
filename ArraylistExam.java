e34d6b5739107eeb8a5c50c407ac18ff20004327
pritish-Development <wadhavanepritish@gmail.com>
2026-08-07 17:37:13 +0530

    06-08 session code

--- ArraylistExam.java
+++ ArraylistExam.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class ArraylistExam {
+    public static void main(String[] args) {
+        ArrayList<String> student = new ArrayList<>();
+
+        student.add("Ankita");
+        student.add("Rajeshwari");
+        student.add("Gayatri");
+        student.add("yogesh");
+        student.add("Gayatri"); // it allows the duplicate element
+
+
+//        System.out.println(student); // print all list
+//        System.out.println(student.get(1)); // get an element by index
+
+//        student.set(2,"Sakshi"); // update an element
+
+//        student.remove(2);
+//        student.remove("yogesh");
+
+//        System.out.println("Number of element in list is :"+student.size());
+
+//        System.out.println("Check if an element exists (true/false) :"+student.contains("Ganesh"));
+
+        System.out.println("isEmpty :"+student.isEmpty());
+
+        for(String temp : student){
+            System.out.println(temp);
+        }
+    }
 }
